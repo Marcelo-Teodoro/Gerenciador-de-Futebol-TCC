2824ebe1476a793daaae035c31d4dcb31394ff63
Pgz <Pgz@Pgz-PC>
2016-05-23 17:56:26 -0300

    Editar, Salvar e Sair no PanelGrid

---
+++ src/java/bean/MemberBean.java
@@ -0,0 +1,178 @@
+package bean;
+
+import dao.MemberDAO;
+import entity.Member;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.ListDataModel;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import org.primefaces.event.SelectEvent;
+import utilities.EnumNivelAcesso;
+import utilities.JSFUtils;
+
+/**
+ * @author Pgz
+ */
+@ManagedBean(name = "MBMember")
+@ViewScoped
+public class MemberBean implements Serializable {
+    
+    private String fechar;
+    private boolean click;
+    private boolean selecionado;
+    private Member member;
+    private ListDataModel<Member> memberList;
+    private EnumNivelAcesso[] enumNivelAcessos;
+    private Member selectedMember;
+
+    public MemberBean(Member member, boolean selecionado, ListDataModel<Member> memberList, EnumNivelAcesso[] enumNivelAcessos) {
+        this.member = member;
+        this.selecionado = selecionado;
+        this.memberList = memberList;
+        this.enumNivelAcessos = enumNivelAcessos;
+    }
+
+    public MemberBean() {
+    }
+
+    public ListDataModel<Member> getMemberList() {
+        return memberList;
+    }
+
+    public void setMemberList(ListDataModel<Member> memberList) {
+        this.memberList = memberList;
+    }
+
+    @PostConstruct
+    public void getAllMember() {
+        prepararMember();
+        try {
+            MemberDAO memberDao = new MemberDAO();
+            List<Member> memberArrayList = memberDao.getAll();
+            memberList = new ListDataModel<>(memberArrayList);
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+            JSFUtils.adicionarMensagemErro(ex.getMessage());
+        }
+        
+    }
+
+    public void prepararMember() {
+        member = new Member();
+    }
+
+    public void saveMember() {
+        try {
+            MemberDAO memberDAO = new MemberDAO();
+            memberDAO.save(member);
+            ArrayList<Member> listMembers = (ArrayList<Member>) memberDAO.getAll();
+            memberList = new ListDataModel<Member>(listMembers);
+            JSFUtils.adicionarMensagemSucesso("Membro salvo com sucesso!!!");
+            fechar = "PF('newMember').hide();";
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+            JSFUtils.adicionarMensagemErro(ex.getMessage());
+            fechar = "";
+        }
+    }
+    
+    public void deleteMember() {
+        try {
+            MemberDAO memberDAO = new MemberDAO();
+            memberDAO.delete(member);
+            ArrayList<Member> listMembers = (ArrayList<Member>) memberDAO.getAll();
+            memberList = new ListDataModel<Member>(listMembers);
+            JSFUtils.adicionarMensagemSucesso("Membro excluído com sucesso!!!");
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+            JSFUtils.adicionarMensagemErro(ex.getMessage());
+        }
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void onRowSelect(SelectEvent event) {
+        member = (Member) event.getObject();
+//        selecionado = true;
+//        setSelecionado(true);
+        System.out.println("Member = " + member.getNomeMember()
+        + " SELECIONADO " + selecionado);
+    }
+
+    public void onRowUnselect(SelectEvent event){
+        selecionado = false;
+    }
+    
+    public EnumNivelAcesso[] getNivel() {
+        
+        return EnumNivelAcesso.values();
+    }
+
+    public EnumNivelAcesso[] getEnumNivelAcessos() {
+        return enumNivelAcessos;
+    }
+
+    public void setEnumNivelAcessos(EnumNivelAcesso[] enumNivelAcessos) {
+        this.enumNivelAcessos = enumNivelAcessos;
+    }
+
+    public boolean isSelecionado() {
+        return selecionado;
+    }
+
+    public void setSelecionado(boolean selecionado) {
+        this.selecionado = selecionado;
+    }
+
+    public String getFormatoBr(Date data) {
+        prepararMember();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(data);
+    }
+    
+    public Member getSelectedMember() {
+        return selectedMember;
+    }
+ 
+    public void setSelectedMember(Member selectedMember) {
+        this.selectedMember = selectedMember;
+    }
+    
+    
+//    public void isClick() {
+//        System.out.println("Clicado");
+//    }
+//    
+
+    public boolean isClick() {
+        System.out.println("Clicado");
+        return click;
+    }
+
+    public void setClick(boolean click) {
+        this.click = click;
+    }
+
+    public String getFechar() {
+        return fechar;
+    }
+
+    public void setFechar(String fechar) {
+        this.fechar = fechar;
+    }
+        
+}
